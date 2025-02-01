f4c3c6504bebe3b8908f78b8a2d47932286e4131
Sii <89252763800@mail.ru>
2025-02-02 01:09:24 +0300

    Added main.py and summator.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from summator import make_sum
+
+
+print('Hello, world!')
---
+++ summator.py
@@ -0,0 +1,9 @@
+def make_sum(a, b):
+    return a + b
+
+
+print(f'{__name__=}')
+if __name__ == '__main__':
+    print(make_sum(5, 8))
+    print(make_sum(-5, 8))
+    print(make_sum(5, -8))
