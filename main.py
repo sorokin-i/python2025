ae80028df1e2716fe7f030de9fe765be8ce8143f
sorokin_i <sorokin_i@1060.ru>
2025-03-03 15:12:57 +0300

    started bot development

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import requests
+import logging
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 from config import TOKEN
 
 
-base_url = f"https://api.telegram.org/bot{TOKEN}/"
-command = "getMe"
-response = requests.get(base_url + command)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)-30s[%(lineno)5d] - %(message)s"
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+log = logging.getLogger(__name__)
 
-print(response.json())
+
+async def hello(update: Update):
+    log.info(f"hello func called by {update.effective_user.full_name}")
+    print()
+
+async def echo(update: Update):
+    log.info(f"echo func called by {update.effective_user.full_name}")
+    print()
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(MessageHandler(filters.ALL, echo))
+
+app.run_polling()
