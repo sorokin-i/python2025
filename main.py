16164b615d2347f8d55255ccc14ea30253c07587
sorokin_i <sorokin_i@1060.ru>
2025-03-17 15:18:19 +0300

    Добавлена функция help и показ клавиатуры

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes, MessageHandler, filters
+from telegram.constants import ParseMode
 
 from config import TOKEN
 
@@ -16,17 +17,31 @@ log = logging.getLogger(__name__)
 
 async def hello(update: Update, context: ContextTypes):
     user = update.effective_user
-    log.info(f"hello func called by {user.full_name}")
-    await update.message.reply_text(f"Привет, {user.full_name}!")
+    log.info(f"hello func called by {user}")
+    await update.message.reply_text(f"<b>Привет,</b> {user.full_name}!", parse_mode=ParseMode.HTML)
 
-
-async def echo(update: Update, context: ContextTypes):
+async def help(update: Update, context: ContextTypes):
     user = update.effective_user
-    log.info(f"echo func called by {user.full_name}")
-    await update.message.reply_text(update.message.text)
+    log.info(f"help func called by {user}")
+    text = "\n".join([
+        "Привет! Я школьный бот. Я умею выполнять следующие команды:",
+        "/start /help - Пришлю все доступные команды",
+        "/hello - Поздороваюсь с тобой",
+        "/show_keyboard - Пришлю клавиатуру",
+    ])
+    await update.message.reply_text(text)
+
+async def show_keyboard(update: Update, context: ContextTypes):
+    user = update.effective_user
+    log.info(f"help func called by {user}")
+    text = "Нажми на кнопку!"
+    buttons = []
+    keyboard = InlineKeyboardMarkup([InlineKeyboardButton("OooooO")])
+    await update.message.reply_text(text, reply_markup=keyboard)
 
 app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler(["start", "help"], help))
 app.add_handler(CommandHandler("hello", hello))
-app.add_handler(MessageHandler(filters.ALL, echo))
+app.add_handler(CommandHandler("show_keyboard", show_keyboard))
 
 app.run_polling()
